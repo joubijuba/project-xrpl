6655ab1ace20e1523dc31c911b34a23e7c251b6a
joubijuba <juba_ramdani@hotmail.com>
2023-07-22 14:10:09 +0200

    changed env variable to remove db url from being written in mongoClient.ts

--- server/src/clients/mongoClient.ts
+++ server/src/clients/mongoClient.ts
@@ -2,13 +2,13 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 
 require("dotenv").config();
 
-const { MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
-const uri: string = `mongodb+srv://${MONGODB_USERNAME}:<${MONGODB_PASSWORD}>@mobirent-v0-db.at8cghd.mongodb.net/?retryWrites=true&w=majority`;
+const { MONGODB_DB_URL, MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
+const uri: string = `mongodb+srv://${MONGODB_USERNAME}:<${MONGODB_PASSWORD}>${MONGODB_DB_URL}/?retryWrites=true&w=majority`;
 
 export const mongoClient = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-})
+  },
+});
